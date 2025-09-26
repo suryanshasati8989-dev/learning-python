da11763479520c08fa3ebb8abc28e34bd17bcd6d
suryansh <suryanshasati@gmil.com>
2025-09-26 09:02:35 +0530

    first commit

---
+++ global vs loval variable.py
@@ -0,0 +1,16 @@
+# x = 'awesome' #global
+# def myfunc():
+#     x = 'fantastic'
+#     print("python is "+x)
+
+# myfunc()
+# print('python is ' + x)
+
+#global keyword
+# def myfile():
+#     global x
+#     x = 'fantastic'
+#     print(x)
+
+# myfile()
+# print(x)
---
+++ suryansh.py
@@ -0,0 +1,15 @@
+# a=6
+# b=9
+# print  ('before suaping:',a,b)
+# a = a+b
+# b = a-b
+# a = a-b
+# print('after swaping',a,b)
+# with using third variable
+a=3
+b=4
+print('before swaping',a,b)
+c=a
+a=b
+b=c
+print(a,b)
