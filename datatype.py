8071c7a4fabfaab73da83d2b5d2dffe6ea0a6394
suryansh <suryanshasati@gmil.com>
2025-10-10 08:34:29 +0530

    suryansh

--- datatype.py
+++ datatype.py
@@ -1,105 +1,171 @@
-<<<<<<< HEAD
-fruits = ["apple","pineapple","mango",]
-print(fruits)
-print(fruits[0])
-print(fruits[-1])
-print(fruits[1:2])
-fruits[0]="kiwi"
-print(fruits)
-fruits.append("babnana")
-print(fruits)
 
-#tuple
-'''tuple = ("ram","sita","hanuman")
-print(tuple)
-print(tuple[0])
-print(len(tuple))
-tuple[0]="radha"
-R = range(7)
-print(R)'''
+# fruits = ["apple","pineapple","mango",]
+# print(fruits)
+# print(fruits[0])
+# print(fruits[-1])
+# print(fruits[1:2])
+# fruits[0]="kiwi"
+# print(fruits)
+# fruits.append("babnana")
+# print(fruits)
 
-#Dictanary
+# #tuple
+# '''tuple = ("ram","sita","hanuman")
+# print(tuple)
+# print(tuple[0])
+# print(len(tuple))
+# tuple[0]="radha"
+# R = range(7)
+# print(R)'''
 
-person = {"ram":"sita","krishna":"radha",}
-print(person)
-print(person["ram"])
+# #Dictanary
 
-#sets
+# person = {"ram":"sita","krishna":"radha",}
+# print(person)
+# print(person["ram"])
 
-set = {12,"suryansh","divyansh",12,34,-12}
-print(set)
-set = frozenset([12,34,56,"yesh"])
-print(set)
-print(10<2)
+# #sets
 
-#string
+# set = {12,"suryansh","divyansh",12,34,-12}
+# print(set)
+# set = frozenset([12,34,56,"yesh"])
+# print(set)
+# print(10<2)
 
-a="hellow world#"
-print(a[2:5])
+# #string
 
-for x in "banana":
-    print(x)
+# a="hellow world#"
+# print(a[2:5])
 
-print(len(a))
-a = " My Name , Is Ram And ,A am studying       "
-print("the" not in a)
+# for x in "banana":
+#     print(x)
 
-#Slicing
+# print(len(a))
+# a = " My Name , Is Ram And ,A am studying       "
+# print("the" not in a)
 
-print(a[2:7])
-print(a[2:12:2])
-print(a[14:1:-1])
-print(a.split(","))
-=======
-fruits = ["apple","pineapple","mango",]
-print(fruits)
-print(fruits[0])
-print(fruits[-1])
-print(fruits[1:2])
-fruits[0]="kiwi"
-print(fruits)
-fruits.append("babnana")
-print(fruits)
+# #Slicing
 
-#tuple
-'''tuple = ("ram","sita","hanuman")
-print(tuple)
-print(tuple[0])
-print(len(tuple))
-tuple[0]="radha"
-R = range(7)
-print(R)'''
+# print(a[2:7])
+# print(a[2:12:2])
+# print(a[14:1:-1])
+# print(a.split(",")
+# fruits = ["apple","pineapple","mango",]
+# print(fruits)
+# print(fruits[0])
+# print(fruits[-1])
+# print(fruits[1:2])
+# fruits[0]="kiwi"
+# print(fruits)
+# fruits.append("babnana")
+# print(fruits)
 
-#Dictanary
+# #tuple
+# '''tuple = ("ram","sita","hanuman")
+# print(tuple)
+# print(tuple[0])
+# print(len(tuple))
+# tuple[0]="radha"
+# R = range(7)
+# print(R)'''
 
-person = {"ram":"sita","krishna":"radha",}
-print(person)
-print(person["ram"])
+# #Dictanary
 
-#sets
+# person = {"ram":"sita","krishna":"radha",}
+# print(person)
+# print(person["ram"])
 
-set = {12,"suryansh","divyansh",12,34,-12}
-print(set)
-set = frozenset([12,34,56,"yesh"])
-print(set)
-print(10<2)
+# #sets
 
-#string
+# set = {12,"suryansh","divyansh",12,34,-12}
+# print(set)
+# set = frozenset([12,34,56,"yesh"])
+# print(set)
+# print(10<2)
 
-a="hellow world#"
-print(a[2:5])
+# #string
 
-for x in "banana":
-    print(x)
+# a="hellow world#"
+# print(a[2:5])
 
-print(len(a))
-a = " My Name , Is Ram And ,A am studying       "
-print("the" not in a)
+# for x in "banana":
+#     print(x)
 
-#Slicing
+# print(len(a))
+# a = " My Name , Is Ram And ,A am studying       "
+# print("the" not in a)
 
-print(a[2:7])
-print(a[2:12:2])
-print(a[14:1:-1])
-print(a.split(","))
->>>>>>> 067572088bd55165fa8965244f0aa9849853a61b
+# #Slicing
+
+# print(a[2:7])
+# print(a[2:12:2])
+# print(a[14:1:-1])
+# print(a.split(","))
+
+
+# for i in range(1,6):
+#     print(i*i)
+# a = int(input("enter number"))
+
+# for i in range(0,a,2):
+#      print(i)
+    
+# a = int(input("enter number "))
+# for i in range(1,a+1):
+#      print(i*2)
+
+# i=1
+# a = int(input("enter your no :"))
+# while i <= a:
+#     print(i*2)
+#     i+=1
+     
+
+# i=1
+# a = int(input("enter your no :"))
+# while i <= a:
+#     print((i*2)-1)
+#     i+=1
+
+# a = int(input("enter number "))
+# for i in range(1,a+1):
+#      print((i*2)-1)
+# sum=0
+# a = int(input("enter your no:"))
+# for i in range(1,a+1):
+#     sum+=i
+# print(sum)
+
+# sum = 0
+# a = int(input("enter your no:"))
+# while 0 <= a:
+#     sum+=1
+# print(summ)
+
+
+# a = int(input('enter your no:'))35
+# rev = 0
+# while a>0:
+#     digit = a%10  d=3
+#     rev = rev*10 + digit   r=5*10+3
+#     a=a//10   a=0
+# print(rev)
+
+    
+
+# a = int(input("enter your no:"))
+# a = str(a)
+# rev = 0
+# for i in a:
+#     a=int(a)
+#     digit = a%10
+#     rev = rev*10 + digit
+#     a=a//10
+# print(rev)
+
+
+a = (input('enter your no:'))
+rev = ''
+for i in (a):
+    rev = (i) + rev
+    print(rev)
