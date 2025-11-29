1f3c63e2f0bc60a0cd1b589038953de89236932e
suryanshasati8989-dev <suryanshasati8989@gmail.com>
2025-11-28 21:08:21 -0800

    Add files via upload

--- tuple.py
+++ tuple.py
@@ -1,9 +1,41 @@
-tuple = ('raman',"kamlash",23,45,76)
-print(type(tuple))
-for i in tuple:
-    print(i)
+# tuple = ('raman',"kamlash",23,45,76)
+# print(type(tuple))
+# for i in tuple:
+#     print(i)
 
-if 'rman' in tuple:
-    print("yes")
+# if 'rman' in tuple:
+#     print("yes")
 
-print(tuple[1:4])
+# print(tuple[1:4])
+
+
+#PYTHON TUPLE : ACCESSING ITEMS
+
+#INDEXING(POSITIVE)
+# Thistuple = ('apple','banana','cherry')
+# print(Thistuple[2])
+
+#INDEXING(indexing)
+# Thistuple = ('apple','banana','cherry')
+# print(Thistuple[-2])
+
+# Range of indexes(Slicing)
+# t = ('a','b','c','d','e','f')
+# print(t[2:5])
+
+#range of negative indexing
+# t = ('a','b','c','d','e','f')
+# print(t[::-1])
+
+#check if item exists
+# t = ('a','b','c','d','e','f')
+# if 'a'in t:
+#     print("yes")
+
+# python tuple values
+t = ('a','b','c','d','e','f')
+l = list(t)
+l[1]='er'
+t = tuple(l)
+print(l)
+
